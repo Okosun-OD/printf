126624d96d1e993e80ffea4b770fc41415866a24
Daniel Okosun <okosunsunogieva@gmail.com>
2022-11-21 08:01:33 -0800

    Updated Repo

---
+++ main.h
@@ -0,0 +1,29 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+  * struct cformat - Short description
+  * @print: argument to print
+  * @p: Pointer
+  *
+  */
+
+typedef struct cformat
+{
+	char *print;
+	int (*p)();
+} cf_t;
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int pc(va_list arg);
+int ps(va_list arg);
+int pd(va_list arg);
+int pi(va_list arg);
+
+#endif
